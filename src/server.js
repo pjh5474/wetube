d86e9ad605338a381bb10d384b0e0033d9427382
pjh5474 <warwarsn@gmail.com>
2023-02-21 01:03:24 +0900

    Middleware Recap

--- src/server.js
+++ src/server.js
@@ -3,29 +3,17 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const logger = (req, res, next) => {
-	console.log(` ${req.method}: ${req.url}`);
+const pathLogger = (req, res, next) => {
+	console.log("PATH", req.path);
 	next();
 };
-const privateMiddleware = (req, res, next) => {
-	const url = req.url;
-	if (url === "/protected") {
-		return res.send("<h1>Not Allowed</h1>");
-	}
-	console.log("Allowed, you may continue.");
+const methodLogger = (req, res, next) => {
+	console.log("METHOD", req.method);
 	next();
 };
 
-const handleProtected = (req, res) => {
-	return res.send("Welcome to the private lounge.");
-};
-
-app.use(logger);
-app.use(privateMiddleware);
-app.get("/", (req, res) => {
-	return res.send("I love middlewares");
-});
-app.get("/protected", handleProtected);
+app.use(methodLogger, pathLogger);
+app.get("/", (req, res) => res.send("I love middlewares"));
 
 const handleListening = () => {
 	console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
