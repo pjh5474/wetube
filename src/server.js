41181379846d1667e62e2beafa022fbf03eba49b
pjh5474 <warwarsn@gmail.com>
2023-02-21 00:31:11 +0900

    Middlewares 20230221

--- src/server.js
+++ src/server.js
@@ -3,15 +3,30 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-app.get("/", (req, res) => {
-	return res.send("I still love you");
-});
-
-const handleLogin = (req, res) => {
-	return res.send("Login here");
+const logger = (req, res, next) => {
+	console.log(` ${req.method}: ${req.url}`);
+	next();
+};
+const privateMiddleware = (req, res, next) => {
+	const url = req.url;
+	if (url === "/protected") {
+		return res.send("<h1>Not Allowed</h1>");
+	}
+	console.log("Allowed, you may continue.");
+	next();
 };
 
-app.get("/login", handleLogin);
+const handleProtected = (req, res) => {
+	return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", (req, res) => {
+	return res.send("I love middlewares");
+});
+app.get("/protected", handleProtected);
+
 const handleListening = () => {
 	console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 };
