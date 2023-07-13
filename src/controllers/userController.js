a55d8e22abdd54dab4892fb742535b53d7b76abd
pjh5474 <warwarsn@gmail.com>
2023-07-13 17:47:01 +0900

    20230713 edit user profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,7 @@ import fetch from "cross-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) =>
-	res.render("join", {
+	res.render("users/join", {
 		pageTitle: "Join",
 	});
 
@@ -12,14 +12,16 @@ export const postJoin = async (req, res) => {
 	const { name, username, email, password, password2, location } = req.body;
 	const pageTitle = "Join";
 	if (password !== password2) {
-		return res.status(400).render("join", {
+		return res.status(400).render("users/join", {
 			pageTitle,
 			errorMessage: "Password confirmation does not match.",
 		});
 	}
-	const exists = await User.exists({ $or: [{ username }, { email }] });
+	const exists = await User.exists({
+		$or: [{ username }, { email }],
+	});
 	if (exists) {
-		return res.status(400).render("join", {
+		return res.status(400).render("users/join", {
 			pageTitle,
 			errorMessage: "This username/email is already taken.",
 		});
@@ -34,7 +36,8 @@ export const postJoin = async (req, res) => {
 		});
 		return res.redirect("/login");
 	} catch (error) {
-		return res.status(400).render("join", {
+		console.log(error);
+		return res.status(400).render("users/join", {
 			pageTitle,
 			errorMessage: error._message,
 		});
@@ -42,21 +45,21 @@ export const postJoin = async (req, res) => {
 };
 
 export const getLogin = (req, res) =>
-	res.render("login", { pageTitle: "Login" });
+	res.render("users/login", { pageTitle: "Login" });
 
 export const postLogin = async (req, res) => {
 	const pageTitle = "Login";
 	const { username, password } = req.body;
-	const user = await User.findOne({ username });
+	const user = await User.findOne({ username, socialOnly: false });
 	if (!user) {
-		return res.status(400).render("login", {
+		return res.status(400).render("users/login", {
 			pageTitle,
 			errorMessage: "An account with this username does not exists.",
 		});
 	}
 	const ok = await bcrypt.compare(password, user.password);
 	if (!ok) {
-		return res.status(400).render("login", {
+		return res.status(400).render("users/login", {
 			pageTitle,
 			errorMessage: "Wrong password",
 		});
@@ -118,13 +121,10 @@ export const finishGithubLogin = async (req, res) => {
 		if (!emailObj) {
 			return res.redirect("/login");
 		}
-		const existingUser = await User.findOne({ email: emailObj.email });
-		if (existingUser) {
-			req.session.loggedIn = true;
-			req.session.user = existingUser;
-			return res.redirect("/");
-		} else {
-			const user = await User.create({
+		let user = await User.findOne({ email: emailObj.email });
+		if (!user) {
+			user = await User.create({
+				avatarUrl: userData.avatar_url,
 				name: userData.name ? userData.name : "Unknown",
 				username: userData.login,
 				email: emailObj.email,
@@ -132,16 +132,99 @@ export const finishGithubLogin = async (req, res) => {
 				socialOnly: true,
 				location: userData.location,
 			});
-			req.session.loggedIn = true;
-			req.session.user = user;
-			return res.redirect("/");
 		}
+		req.session.loggedIn = true;
+		req.session.user = user;
+		return res.redirect("/");
 	} else {
 		return res.redirect("/login");
 	}
 };
 
-export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Log out");
+export const logout = (req, res) => {
+	req.session.destroy();
+	return res.redirect("/");
+};
+
+export const getEdit = (req, res) => {
+	return res.render("users/edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = async (req, res) => {
+	const {
+		session: {
+			user: { _id },
+		},
+		body: { name, email, username, location },
+	} = req;
+	const nameExists = await User.exists({ username });
+	const emailExists = await User.exists({ email });
+	const usernameChanged = req.session.user.username !== username;
+	const emailChanged = req.session.user.email !== email;
+	if (nameExists && usernameChanged) {
+		return res.status(400).render("users/edit-profile", {
+			pageTitle: "Edit Profile",
+			errorMessage: "This username is already taken.",
+		});
+	}
+	if (emailExists && emailChanged) {
+		return res.status(400).render("users/edit-profile", {
+			pageTitle: "Edit Profile",
+			errorMessage: "This email is already taken.",
+		});
+	}
+
+	const updatedUser = await User.findByIdAndUpdate(
+		_id,
+		{
+			name,
+			email,
+			username,
+			location,
+		},
+		{ new: true }
+	);
+	// req.session.user = {
+	// 	...req.session.user,
+	// 	name,
+	// 	email,
+	// 	username,
+	// 	location,
+	// };
+	req.session.user = updatedUser;
+	return res.redirect("/users/edit");
+};
+
+export const getChangePassword = (req, res) => {
+	if (req.session.user.socialOnly === true) {
+		return res.redirect("/");
+	}
+	return res.render("users/change-password", { pageTitle: "Change Password" });
+};
+
+export const postChangePassword = async (req, res) => {
+	const {
+		session: {
+			user: { _id },
+		},
+		body: { oldPassword, newPassword, newPasswordConfirmation },
+	} = req;
+	const user = await User.findById(_id);
+	const ok = await bcrypt.compare(oldPassword, user.password);
+	if (!ok) {
+		return res.status(400).render("users/change-password", {
+			pageTitle: "Change Password",
+			errorMessage: "The current password is incorrect",
+		});
+	}
+	if (newPassword !== newPasswordConfirmation) {
+		return res.status(400).render("users/change-password", {
+			pageTitle: "Change Password",
+			errorMessage: "The password does not match the confirmation",
+		});
+	}
+	user.password = newPassword;
+	await user.save();
+
+	return res.redirect("/users/logout");
+};
 export const see = (req, res) => res.send("See User");
--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
 	name: { type: String, required: true },
+	avatarUrl: String,
 	socialOnly: { type: Boolean, default: false },
 	email: { type: String, required: true, unique: true },
 	username: { type: String, required: true, unique: true },
